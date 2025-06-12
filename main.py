8fe70910cd92ed75170dbef7de8246d05f471f10
Mikcec <michael.cecchini88@gmail.com>
2025-06-12 17:02:42 +0200

    CH1 L4

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+from google import genai
+from dotenv import load_dotenv
+import sys
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+
+    if len(sys.argv) <2:
+        print("Prompt error, no string provided")
+        sys.exit(1)
+    else:
+        argument= sys.argv[1:]
+        
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=argument,
+        )
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+        print("Response:")
+        print(response.text)
+
+
+if __name__ == "__main__":
+    main()
